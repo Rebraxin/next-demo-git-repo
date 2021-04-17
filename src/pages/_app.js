ee97efa3bd7d44edc07d514e3f7ed6801d0fd1aa
Rebraxin <cedric.paje@gmail.com>
2021-04-17 12:39:05 +0200

    Nprogress added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,28 @@
 import Navbar from '../components/navbar'
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import '../scss/style.scss'
+import Router from 'next/dist/next-server/lib/router/router'
+
+NProgress.configure({
+  parent: '#nproPos',
+  showSpinner: false,
+  easing: 'ease',
+  speed: 300,
+  trickleRate: 0.03,
+  trickleSpeed: 300,
+})
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
 
 const MyApp = ({ Component, pageProps }) => {
   return (
@@ -9,6 +31,7 @@ const MyApp = ({ Component, pageProps }) => {
         <title>Github Repositories</title>
       </Head>
       <Navbar />
+      <div style={{ width: '100%', height: '0.5rem' }} id="nproPos" />
       <div className="page">
         <Component {...pageProps} />
       </div>
